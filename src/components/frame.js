ee357bffaf0f1a620c44e778f9942f612ddf3597
Mark England <mark@Marks-Macbook-Pro.local>
2021-07-24 22:14:36 -0400

    Finished Gatsby Tutorial Part 2

---
+++ src/components/frame.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Frame = ({ children }) => {
+  return (
+    <div>
+      <h1>This is the page title</h1>
+      { children }
+    </div>
+  )
+}
+
+export default Frame
